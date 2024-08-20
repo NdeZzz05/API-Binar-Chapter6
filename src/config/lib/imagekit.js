ec16b355adbd56e8b1ec3740800517b0a0c074da
NdeZzz05 <muhammadfernandes05@gmail.com>
2024-08-21 00:02:15 +0700

    add log imagekit

--- src/config/lib/imagekit.js
+++ src/config/lib/imagekit.js
@@ -1,3 +1,7 @@
+console.log("PUBLIC_KEY_IMAGE_KIT:", process.env.PUBLIC_KEY_IMAGE_KIT);
+console.log("PRIVATE_KEY_IMAGE_KIT:", process.env.PRIVATE_KEY_IMAGE_KIT);
+console.log("URL_ENDPOINT_IMAGE_KIT:", process.env.URL_ENDPOINT_IMAGE_KIT);
+
 const ImageKit = require("imagekit");
 
 const imageKitConfig = new ImageKit({
