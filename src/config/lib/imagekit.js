b9b38525d518f79342015cf6e2d49cd9774ff95a
NdeZzz05 <muhammadfernandes05@gmail.com>
2024-08-21 06:45:31 +0700

    delet log imagekit

--- src/config/lib/imagekit.js
+++ src/config/lib/imagekit.js
@@ -1,7 +1,3 @@
-console.log("PUBLIC_KEY_IMAGE_KIT:", process.env.PUBLIC_KEY_IMAGE_KIT);
-console.log("PRIVATE_KEY_IMAGE_KIT:", process.env.PRIVATE_KEY_IMAGE_KIT);
-console.log("URL_ENDPOINT_IMAGE_KIT:", process.env.URL_ENDPOINT_IMAGE_KIT);
-
 const ImageKit = require("imagekit");
 
 const imageKitConfig = new ImageKit({
