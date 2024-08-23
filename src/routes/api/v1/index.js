0f8e9319382d16271c36c18d8d12a4a4d965fd5a
NdeZzz05 <muhammadfernandes05@gmail.com>
2024-08-23 23:06:06 +0700

    add docs api swagger

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -4,10 +4,12 @@ const AUTH_ROUTER = require("./auth/index");
 const USER_ROUTER = require("./user/index");
 const CATEGORY_ROUTER = require("./category/index");
 const POST_ROUTER = require("./post/index");
+const DOCS_ROUTER = require("./docs/index");
 
 router.use("/auth", AUTH_ROUTER);
 router.use("/users", USER_ROUTER);
 router.use("/categories", CATEGORY_ROUTER);
 router.use("/post", POST_ROUTER);
+router.use("/docs", DOCS_ROUTER);
 
 module.exports = router;
