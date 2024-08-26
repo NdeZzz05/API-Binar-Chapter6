a3f8481af1395a42564a34c63af0f521afccbe62
NdeZzz05 <muhammadfernandes05@gmail.com>
2024-08-26 21:11:16 +0700

    add sentry for logging

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+require("./config/sentry");
+const Sentry = require("@sentry/node");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
@@ -9,7 +11,7 @@ const ERROR_HANDLER = require("./middleware/errorHandling.middleware");
 const NOT_FOUND_HANDLER = require("./middleware/notFoundHandling.middleware");
 const cors = require("cors");
 
-app.set("view engine", "ejs"); // Tambahkan ini
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(logger("dev"));
@@ -19,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(INDEX_ROUTES);
+Sentry.setupExpressErrorHandler(app);
 app.use(NOT_FOUND_HANDLER);
 app.use(ERROR_HANDLER);
 
